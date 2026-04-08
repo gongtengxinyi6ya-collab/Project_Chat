8b8a6b995a1e20d4eb9b17febd0bf1e9b016330c
jason <jason@example.com>
2026-04-08 17:19:23 +0800

    增加StderrSink,指出日志输出到终端

--- include/StderrSink.h
+++ include/StderrSink.h
@@ -1,9 +1,26 @@
 #include <unistd.h>
+#include <errno.h>
 #include "LogSink.h"
 //继承LogSink,实现write方法，将日志输出到标准错误流
 class StderrSink:public LogSink{
 public:
     void write(std::string_view line) override{
-        ::write(STDERR_FILENO,line.data(),line.size());
+        const char* p=line.data();
+        size_t left=line.size();//
+        while(left>0){
+            ssize_t n=::write(2,p,left);
+            if(n>0){
+                p+=n;
+                left-=n;
+            }
+            if(n==-1){
+                if(errno==EINTR){
+                    continue;
+                }else{
+                    break;
+                }
+            }
+        }
+        
     }
 };
--- server/server.cpp
+++ server/server.cpp
@@ -1,11 +1,12 @@
 #include "TcpServer.h"
 #include "Logger.h"
 #include "FileSink.h"
+#include "StderrSink.h"
 #include <iostream>
 int main()
 {
     Logger::instance().setLevel(LogLevel::DEBUG);
-    Logger::instance().setSink(std::make_unique<FileSink>("server.log"));
+    Logger::instance().setSink(std::make_unique<StderrSink>());
     EventLoop loop;
     TcpServer server(&loop,8080);
     server.setThreadNum(4);
