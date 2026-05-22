71407ce35f087af77fd12168f19fcdfac2ae1609
jason <jason@example.com>
2026-05-22 16:04:27 +0800

    添加PasswordHasher进行哈希映射和哈希校验

---
+++ include/security/PasswordHasher.h
@@ -0,0 +1,23 @@
+#include <string>
+/*
+负责密码哈希和密码校验*/
+namespace security{
+//保存一次密码哈希的结果
+struct PasswordHash{
+    std::string hash;
+    std::string salt;
+};
+
+class PasswordHasher{
+public:
+    PasswordHasher(size_t saltLength=16,const std::string& algorithm);
+    PasswordHash hashPassword(const std::string& password);//用户注册时调用，输出salt+hash
+    bool verifyPassword(const std::string& password,const std::string& expextedHash,const std::string& salt);//登录时调用，对用户密码进行计算后与数据库中的哈希密码进行校验
+    std::string generateSalt();//生成随机salt
+    std::string calculateHash(const std::string& password,const std::string& salt);//根据密码和salt计算哈希值
+    
+private:
+    size_t saltLength_;//salt长度
+    std::string algorithm_;//当前算法名称
+};
+}
---
+++ src/security/PasswordHasher.cpp
@@ -0,0 +1,51 @@
+#include <openssl/sha.h>
+#include <openssl/rand.h>
+#include <stdexcept>
+#include <vector>
+#include <sstream>
+#include <iomanip>
+#include "security/PasswordHasher.h"
+
+security::PasswordHasher::PasswordHasher(size_t saltLength,const std::string& algorithm)
+:saltLength_(saltLength),algorithm_(algorithm){
+
+}
+
+security::PasswordHash security::PasswordHasher::hashPassword(const std::string& password){
+    if(password.empty()){
+        throw std::invalid_argument("password is empty");
+    }
+    std::string salt=generateSalt();
+    std::string hash=calculateHash(password,salt);
+    return PasswordHash{.hash=hash,.salt=salt};
+}
+bool security::PasswordHasher::verifyPassword(const std::string& password,const std::string& expectedHash,const std::string&salt){
+    if(password.empty()||expectedHash.empty()||salt.empty()){
+        return false;
+    }
+    std::string hash=calculateHash(password,salt);
+    return hash==expectedHash;
+}
+std::string security::PasswordHasher::generateSalt(){
+    std::vector<unsigned char> buf(saltLength_);
+    if(RAND_bytes(buf.data(),buf.size())!=1){
+        //生成len字节密码学安全随机数
+        throw std::runtime_error("RAND_bytes failed");
+    }
+    //将二进制数据转换为十六进制字符串
+    std::stringstream ss;
+    for(auto b:buf){
+        ss
+        <<std::hex//后续整数按16进制输出
+        <<static_cast<int>(b)//转化为整数
+        <<std::setw(2)//输出宽度至少为2
+        <<std::setfill('0');//宽度不足补充0
+    }
+    return ss.str();
+}
+std::string security::PasswordHasher::calculateHash(const std::string& password,const std::string&salt){
+    if(password.empty()||salt.empty()){
+        return "";
+    }
+    
+}
