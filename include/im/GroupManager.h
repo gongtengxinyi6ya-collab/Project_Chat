70a1eea73e7d73a915aec5ba9b971767d2984952
jason <jason@example.com>
2026-05-30 17:12:48 +0800

    回退改动

--- include/im/GroupManager.h
+++ include/im/GroupManager.h
@@ -20,10 +20,7 @@ enum class QuitResult{
     ERR_NO_SUCH_GROUP
 };
 class GroupManager{
-    struct GroupMember{
-        std::string accountId;
-        std::string userName;
-    };
+
 public:
     std::pair<bool,std::string> createGroup(const std::string& owner,const std::string& name);//创建群并让owner自动入群
     JoinResult joinGroup(const std::string &groupId,const std::string& user);//用户入群
@@ -33,7 +30,7 @@ public:
     std::vector<std::string> groupsOfUser(const std::string& user)const;//取用户加入的所有群
     bool isMember(const std::string& groupId,const std::string& user) const;//群存在且成员包含user
     bool exists(const std::string& groupId)const;//group是否存在
-    bool restoreGroup(const std::string& groupId,const std::string& groupName,const std::string& owner,const std::vector<GroupMember>& members);//启动时恢复群对象和成员集合
+    bool restoreGroup(const std::string& groupId,const std::string& groupName,const std::string& owner,const std::vector<std::string>& members);//启动时恢复群对象和成员集合
 private:
     std::unordered_map<std::string,std::unordered_set<std::string>>  userGroups_;//user映射groupId
     std::unordered_map<std::string,Group> groupsById_;//groupId映射Group主储存
