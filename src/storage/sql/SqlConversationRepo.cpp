2d681da1d6b80a181e9295db41ecbe2d8f5e23b6
jason <jason@example.com>
2026-06-05 11:49:12 +0800

    实现更新会话行方法

--- src/storage/sql/SqlConversationRepo.cpp
+++ src/storage/sql/SqlConversationRepo.cpp
@@ -3,6 +3,7 @@
 #include "storage/sql/SqlConnectionPool.h"
 #include "storage/sql/SqlConnectionGuard.h"
 #include "storage/sql/SqlTransaction.h"
+#include <stdexcept>
 storage::SqlConversationRepo::SqlConversationRepo(std::shared_ptr<SqlConnectionPool> pool)
 :pool_(std::move(pool)){
 
@@ -16,36 +17,55 @@ storage::RepoResult storage::SqlConversationRepo::upserDirectOnMessage(const std
     }
     auto conn=pool_->acquire();
     if(!conn||!conn->connected()){
-        return {.status=RepoStatus::Internal,"Failed to connect the dataBase"};
+        return {.status=RepoStatus::Internal,.message="Failed to connect the dataBase"};
     }
     try{
         //开启事务
-        SqlTransaction transation;
-        auto result1=conn->executePrepared("INSERT INTO conversations (
-    owner_account_id,
-    conversation_type,
-    target_id,
-    last_msg_id,
-    last_preview,
-    last_sender_account_id,
-    last_sender_username,
-    last_ts_ms,
-    unread_count,
-    last_read_msg_id,
-    last_read_at_ms
-)
-VALUES (?, 1, ?, ?, ?, ?, ?, ?, 0, ?, ?)
-ON DUPLICATE KEY UPDATE
-    last_msg_id = VALUES(last_msg_id),
-    last_preview = VALUES(last_preview),
-    last_sender_account_id = VALUES(last_sender_account_id),
-    last_sender_username = VALUES(last_sender_username),
-    last_ts_ms = VALUES(last_ts_ms),
-    unread_count = 0,
-    last_read_msg_id = VALUES(last_read_msg_id),
-    last_read_at_ms = VALUES(last_read_at_ms);",{senderAccountId,receiverAccountId,msgId,preview,senderAccountId,senderUsername,serverTsMs,msgId,serverTsMs});
-    if(!result1.ok()){
-        return {.status=RepoStatus::SqlError};
-    }
+        SqlTransaction transation(*conn);
+        const std::string sql1=R"(INSERT INTO conversations 
+        (owner_account_id,conversation_type,target_id,last_msg_id,last_preview,last_sender_account_id,last_sender_username,last_ts_ms,unread_count,last_read_msg_id,last_read_at_ms)
+        VALUES (?, 1, ?, ?, ?, ?, ?, ?, 0, ?, ?)
+        ON DUPLICATE KEY UPDATE
+            last_msg_id = VALUES(last_msg_id),
+            last_preview = VALUES(last_preview),
+            last_sender_account_id = VALUES(last_sender_account_id),
+            last_sender_username = VALUES(last_sender_username),
+            last_ts_ms = VALUES(last_ts_ms),
+            unread_count = 0,
+            last_read_msg_id = VALUES(last_read_msg_id),
+            last_read_at_ms = VALUES(last_read_at_ms)";
+        auto result1=conn->executePrepared(sql1,{senderAccountId,receiverAccountId,msgId,preview,senderAccountId,senderUsername,serverTsMs,msgId,serverTsMs});
+        if(!result1.ok()){
+            return {.status=RepoStatus::SqlError,.message=result1.error};
+        }
+        const std::string sql2=R"(
+        INSERT INTO conversations (
+            owner_account_id,
+            conversation_type,
+            target_id,
+            last_msg_id,
+            ast_preview,
+            last_sender_account_id,
+            last_sender_username,
+            last_ts_ms,
+            unread_count
+        )
+        VALUES (?, 1, ?, ?, ?, ?, ?, ?, 1)
+        ON DUPLICATE KEY UPDATE
+            last_msg_id = VALUES(last_msg_id),
+            last_preview = VALUES(last_preview),
+            last_sender_account_id = VALUES(last_sender_account_id),
+            last_sender_username = VALUES(last_sender_username),
+            last_ts_ms = VALUES(last_ts_ms),
+            unread_count = unread_count + 1;
+        )";
+        auto result2=conn->executePrepared(sql2,{receiverAccountId,senderAccountId,msgId,preview,senderAccountId,senderUsername,serverTsMs});
+        if(!result2.ok()){
+            return {.status=RepoStatus::SqlError,.message=result2.error};
+        }
+        transation.commit();
+        return {.status=RepoStatus::Ok};
+    }catch(const std::exception&e){
+        return {.status=RepoStatus::Internal,.message=e.what()};
     }
 }
